412a1682bf08887f6a0c81349566cf43fbb52655
John Edwards <edwards.aus@hotmail.com>
2021-02-28 12:57:45 +1100

    updated to work with python3 and added more debuggingas well

--- sandcastle.py
+++ sandcastle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys, os, commands, requests
+import sys, os, subprocess, requests
 from argparse import ArgumentParser
 
-print """
+print("""
    ____             __             __  __   
   / __/__ ____  ___/ /______ ____ / /_/ /__ 
  _\ \/ _ `/ _ \/ _  / __/ _ `(_-</ __/ / -_)
@@ -12,7 +12,7 @@ print """
                                             
 S3 bucket enumeration // release v1.2.4 // ysx
 
-"""
+""")
 targetStem = ""
 inputFile = ""
 
@@ -27,17 +27,20 @@ with open(args.inputFile, 'r') as f:
     bucketNames = [line.strip() for line in f] 
     lineCount = len(bucketNames)
 
-print "[*] Commencing enumeration of '%s', reading %i lines from '%s'." % (args.targetStem, lineCount, f.name)
+print("[*] Commencing enumeration of '%s', reading %i lines from '%s'." % (args.targetStem, lineCount, f.name))
 
-for name in bucketNames:
-	r = requests.head("http://%s%s.s3.amazonaws.com" % (args.targetStem, name))
-	if r.status_code != 404:
-                # macOS, coming soon: os.system("notify Potential match found! %s%s: %s" % (args.targetStem, name, r.status_code))
-		print "[+] Checking potential match: %s%s --> %s" % (args.targetStem, name, r.status_code)
-		check = commands.getoutput("/usr/local/bin/aws s3 ls s3://%s%s" % (args.targetStem, name))
-		print check
-	else:
-		sys.stdout.write('')
+for i, name in enumerate(bucketNames):
+        bucket_name = ("%s%s" %(args.targetStem, name))
 
-print "[*] Enumeration of '%s' buckets complete." % (args.targetStem)
+        print("[*] %d/%d: Testing bucket: %s" %(i+1, lineCount, bucket_name))
+        r = requests.head("http://%s.s3.amazonaws.com" % (bucket_name))
+        if r.status_code != 404:
+            # macOS, coming soon: os.system("notify Potential match found! %s%s: %s" % (args.targetStem, name, r.status_code))
+            print("[+] Checking potential match: %s%s --> %s" % (args.targetStem, name, r.status_code))
+            check = subprocess.getoutput("/usr/local/bin/aws s3 ls s3://%s%s" % (args.targetStem, name))
+            print(check)
+        else:
+            sys.stdout.write('')
+
+print("[*] Enumeration of '%s' buckets complete." % (args.targetStem))
 # macOS, coming soon: os.system("notify Enumeration of %s buckets complete." % (args.targetStem))
